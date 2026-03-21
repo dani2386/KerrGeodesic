fixed convergence and precision

src/kerr_solver.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 from src.ode_solver import ODESolver
 
 
@@ -36,23 +37,24 @@ class KerrSolver(ODESolver):
         return super().solve(run_id, t_max, dt, np.array([0, 0, r, pr]), depth, **kwargs)
 
     def plot_trajectory(self, run_id, depth, ax=None, **kwargs):
-        if ax is None: fig, ax = plt.subplots()
-        line = None
+        if ax is None: fig, ax = plt.subplots(figsize=(6, 6))
+        ax.set_xlim(-10, 10)
+        ax.set_ylim(-10, 10)
+        ax.grid(True)
 
-        theta = np.linspace(0, 2 * np.pi, 100)
-        r_bh = 1 + np.sqrt(1 - self.a**2)
-
-        ax.plot(r_bh * np.cos(theta), r_bh * np.sin(theta), color='black')
+        ax.add_patch(Circle((0, 0), 1 + np.sqrt(1 - self.a**2), color='black', linestyle='--', fill=False))
 
         with self._file as file:
             t_max, dt = file.load_metadata(run_id, ('t_max', 'dt'))
             n_max = int(t_max / dt)
+            line = None
 
-            for n in range(0, n_max, depth):
-                buf_len = min(depth, n_max - n)
+            for n in range(0, n_max + 1, depth):
+                buf_len = min(depth, n_max - n + 1)
 
-                phi = file.load(f'{run_id}/states/base', (slice(n, n + buf_len + 1), 1))
-                r = file.load(f'{run_id}/states/base', (slice(n, n + buf_len + 1), 2))
+                phi = file.load(f'{run_id}/states/base', (slice(n, n + buf_len), 1))
+                r = file.load(f'{run_id}/states/base', (slice(n, n + buf_len), 2))
+
                 x = r * np.cos(phi)
                 y = r * np.sin(phi)
 

src/ode_solver.py
@@ -17,25 +17,26 @@ class ODESolver:
         dim = len(init)
 
         with self._file as file:
-            file.create_dataset(dataset, (n_max + 1, dim))
+            file.create_dataset(dataset, (n_max + 1, dim)) # n_max + 1 since t_max is inclusive
             file.save(dataset, 0, init)
 
-            for n in range(0, n_max, depth):
+            for n in range(0, n_max, depth): # n is supposed to go until n_max - 1 because it will be the prev state
                 buf_len = min(depth, n_max - n)
-                buffer = np.empty([buf_len + 1, dim])
-                buffer[0] = file.load(dataset, n)
+                buffer = np.empty((buf_len + 1, dim)) # buffer buf_len + 1 to accommodate the prev state
+                buffer[0] = init if n == 0 else prev
 
                 for i in range(buf_len):
                     t = (n + i) * dt
 
                     k1 = self.f(t, buffer[i])
-                    k2 = self.f(t + 0.5 * dt, buffer[i] + dt / 2 * k1)
-                    k3 = self.f(t + 0.5 * dt, buffer[i] + dt / 2 * k2)
+                    k2 = self.f(t + dt / 2, buffer[i] + dt / 2 * k1)
+                    k3 = self.f(t + dt / 2, buffer[i] + dt / 2 * k2)
                     k4 = self.f(t + dt, buffer[i] + dt * k3)
 
                     buffer[i + 1] = buffer[i] + dt / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
 
                 file.save(dataset, slice(n + 1, n + buf_len + 1), buffer[1:])
+                prev = buffer[-1]
 
     def solve(self, run_id, t_max, dt, init, depth, **kwargs):
         n_max = int(t_max / dt)
@@ -57,8 +58,8 @@ class ODESolver:
             file.create_group(f'{run_id}/conv')
             file.create_dataset(f'{run_id}/conv/{conv}', (2, n_max + 1, dim))
 
-            for n in range(0, n_max, depth):
-                buf_len = min(depth, n_max - n)
+            for n in range(0, n_max + 1, depth):
+                buf_len = min(depth, n_max - n + 1)
                 buffer = [file.load(f'{run_id}/states/{res}', slice(n * 2**i, (n + buf_len) * 2**i, 2**i))
                           for i, res in enumerate(conv_res[conv])]
 
@@ -70,31 +71,32 @@ class ODESolver:
                 else:
                     err_buffer = np.stack([buffer[0] - buffer[1], buffer[1] - buffer[2]])
 
-                file.save(f'{run_id}/conv/{conv}', (slice(None), slice(n, n + buf_len)), err_buffer)
+                file.save(f'{run_id}/conv/{conv}', (slice(None), slice(n, n + buf_len)), np.abs(err_buffer))
 
     def plot(self, run_id, label, depth, ax=None, **kwargs):
         if ax is None: fig, ax = plt.subplots()
-        line = None
+        ax.grid(True)
 
         conv = kwargs.pop('conv', None)
         if conv: ax.set_yscale('log')
 
-        with self._file as file:
+        with (self._file as file):
             t_max, dt = file.load_metadata(run_id, ('t_max', 'dt'))
             n_max = int(t_max / dt)
             idx = self.labels.index(label)
+            line = None
 
-            for n in range(0, n_max, depth):
-                buf_len = min(depth, n_max - n)
+            for n in range(0, n_max + 1, depth):
+                buf_len = min(depth, n_max - n + 1)
 
-                x = np.arange(n, n + buf_len + 1) * dt
-                y = [file.load(f'{run_id}/states/base', (slice(n, n + buf_len + 1), idx))] if not conv \
-                    else file.load(f'{run_id}/conv/{conv}', (slice(None), slice(n, n + buf_len + 1), idx))
+                t = np.arange(n, n + buf_len) * dt
+                data = [file.load(f'{run_id}/states/base', (slice(n, n + buf_len), idx))] if not conv else \
+                        file.load(f'{run_id}/conv/{conv}', (slice(None), slice(n, n + buf_len), idx))
 
                 if line is None:
-                    line = ax.plot(x, np.transpose(y), **kwargs)
+                    line = ax.plot(t, np.transpose(data), **kwargs)
                 else:
-                    for ln, y_sub in zip(line, y):
-                        ax.plot(x, y_sub, color=ln.get_color(), linestyle=ln.get_linestyle(), label=None)
+                    for ln, sub_data in zip(line, data):
+                        ax.plot(t, sub_data, color=ln.get_color(), linestyle=ln.get_linestyle(), label=None)
 
         return ax