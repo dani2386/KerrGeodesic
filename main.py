e97ed68893a769fa6343eeb0cc0b574f4db11323
Random0utlander <ricardo.addc.branco@gmail.com>
2026-03-23 16:56:34 +0000

    Fixed mathematica partial derivatives

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from src.kerr_solver import KerrSolver
 
 
 a = 0.1
-
+r0 = 9
+m,M = 1e-5, 1
+E0=(1-2*M/r0+a*M**0.5/r0**1.5)/(1-3*M/r0+2*a*M**0.5/r0**1.5)**0.5
+L0 = ((M*r0)**0.5*-2*a*M/r0+M**0.5*a**2/r0**2)/(1-3*M/r0+2*a*M**0.5/r0**1.5)**0.5
+params = (180, 0.01, r0, E0, L0)  # (t_max, dt, r, E, J)
 run_id = 'test_run'
-params = (180, 0.01, 10, 0.76, 0.2)  # (t_max, dt, r, E, J)
 depth = 50
 
 def main():
     solver = KerrSolver('bin', a)
+    
 
     print('Solving...')
 
